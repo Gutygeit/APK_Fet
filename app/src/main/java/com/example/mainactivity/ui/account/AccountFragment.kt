532d201fc8dcf39b618d4e071bb88f82eaf85ed0
Tiliphou <andoni.iparraguirre@gmail.com>
2023-06-08 11:18:17 +0200

    profile picture set up

--- app/src/main/java/com/example/mainactivity/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/mainactivity/ui/account/AccountFragment.kt
@@ -1,17 +1,32 @@
 package com.example.mainactivity.ui.account
 
+import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.mainactivity.LoginActivity
 import com.example.mainactivity.databinding.FragmentAccountBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import java.io.ByteArrayOutputStream
+import java.io.File
 
 class AccountFragment : Fragment() {
 
@@ -21,12 +36,25 @@ class AccountFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
     private lateinit var deconnect : Button
-
+    private val pickImage = 100
+    private var imageUri: Uri? = null
+    @SuppressLint("WrongThread")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        val StoRef = FirebaseStorage.getInstance().reference.child("images/tele.jpeg")
+        val localfile = File.createTempFile("tempimage","jpeg")
+        StoRef.getFile(localfile).addOnSuccessListener {
+            val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
+            binding.imageViewpp.setImageBitmap(bitmap)
+        }.addOnFailureListener{
+            Toast.makeText(getActivity(), "This is my Toast message!",
+                Toast.LENGTH_LONG).show();
+        }
+
         val accountViewModel =
             ViewModelProvider(this).get(AccountViewModel::class.java)
 
@@ -34,10 +62,11 @@ class AccountFragment : Fragment() {
         val root: View = binding.root
 
         val textView: TextView = binding.textAccount
-        accountViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = "this is the new account"
+        binding.imageViewpp.setOnClickListener{
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, pickImage)
+            ppclicked()
         }
-
 
         deconnect = binding.deconnect
         deconnect.setOnClickListener{
@@ -53,5 +82,18 @@ class AccountFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun ppclicked(){
+        val textView: TextView = binding.textAccount
+        textView.text = "clicked ?"
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == pickImage) {
+            imageUri = data?.data
+            binding.imageViewpp.setImageURI(imageUri)
+        }
     }
 }
